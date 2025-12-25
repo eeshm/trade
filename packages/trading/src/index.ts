f1d9ce7242978c88b90da5a8dadf2a642fd37ca6
eeshm <eeshmidha80@gmail.com>
2025-12-26 00:11:15 +0530

    chore: center logic for exporting all functions of trading package

--- packages/trading/src/index.ts
+++ packages/trading/src/index.ts
@@ -1,5 +1,30 @@
-export const executeTrade = (symbol: string, quantity: number) => {
-  // TODO: Implement trade execution
-};
+/**
+ * Trading Module
+ * Main barrel export for all trading functionality
+ */
 
-export default {};
+// Constants
+export { ORDER_STATUS, ORDER_SIDE, ORDER_TYPE, ASSETS, FEE_RATE, INITIAL_BALANCE } from './constants.js';
+
+// Validation
+export {
+  validateOrderInput,
+  validateStatusTransition,
+  validateTradeExecution,
+  validateBalance,
+} from './validation.js';
+
+// Fees
+export { calculateFee, validateFee } from './fees.js';
+
+// Positions
+export { initPosition, updatePosition } from './positions.js';
+
+// Orders
+export { placeOrder, getOrder, getUserOrders } from './orders.js';
+
+// Fills (execution)
+export { fillOrder, rejectOrder } from './fills.js';
+
+// Portfolio
+export { initPortfolio, getPortfolio } from './portfolio.js';
