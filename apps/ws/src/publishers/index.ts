63d4884daceb4d3ecaef42a5428a24348641271e
eeshm <eeshmidha80@gmail.com>
2026-01-05 16:17:30 +0530

    feat(ws): export all publishers

--- apps/ws/src/publishers/index.ts
+++ apps/ws/src/publishers/index.ts
@@ -2,5 +2,5 @@
  * Export all publishers
  */
 
-export * from "./prices.ts";
-export * from "./orders.ts";
+export { startPricePublisher } from "./prices.js";
+export { startOrderPublisher } from "./orders.js";
